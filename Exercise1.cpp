978e77468208992c189ffda41fe3495ffe2ad611
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-03 20:34:23 +0500

    Intro of programing language and variable naming rule

---
+++ Exercise1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << endl;
+    cout << "Pascal case syntax each starting latter will be capital no space use between variable name.";
+    cout << "Example of Pascal is 'FirstName'.";
+    cout << "Camel case syntax first word first letter will smalle after that each word start with capital letter no space use between variable name.";
+    cout << "Example of Camel case is 'firstName'.";
+    cout << endl;
+}
