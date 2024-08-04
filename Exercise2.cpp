4c1676b4c3e869d2524521a0477219d0d84c7cba
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-04 14:36:41 +0500

    variables datatypes input&output

---
+++ Exercise2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter the value of num : ";
+    cin >> num;
+    cout << "The value of num is " << num << endl;
+
+    string firstName, lastName;
+    cout << "Enter your first name : ";
+    cin >> firstName;
+    cout << "Enter your last name : ";
+    cin >> lastName;
+
+    cout << "Your full name is " << firstName << " " << lastName << endl;
+    return 0;
+}
