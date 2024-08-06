7e4a277b6b812bd602aa63ea01679e1964c05e48
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-06 19:25:38 +0500

    For Loop.

---
+++ Exercise15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter the number to print your name xTimes : ";
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cout << "Yasir Ali " << i << endl;
+    }
+    return 0;
+}
