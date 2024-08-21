637d56726e4fe414f5f7e44b1fccb9363e87073b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-21 19:17:48 +0500

    Quick Sort

---
+++ Exercise88.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// Function to swap two elements
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Function to partition the array on the basis of pivot
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high]; // pivot
+    int i = (low - 1);     // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If the current element is smaller than or equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++; // Increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Function that implements QuickSort
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        // pi is partitioning index, arr[p] is now at right place
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    return 0;
+}
